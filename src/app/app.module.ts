a0c20d01a1179dbf6b456b6870093927a55678af
yigityalin <yigityalin02@gmail.com>
2022-06-20 12:52:11 +0300

    removed project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ProjectsComponent} from "./projects/projects.component";
-import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsComponent,
-    ProjectComponent
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Project} from "./project";
 
 @Component({
   selector: 'app-projects',
@@ -7,4 +8,11 @@ import {Component} from '@angular/core';
 })
 export class ProjectsComponent {
   title = 'Projects';
+  projects: Project[] = [];
+
+  constructor() {
+    for (let i = 0; i < 5; i++) {
+      this.projects.push(new Project('Project ' + i))
+    }
+  }
 }
